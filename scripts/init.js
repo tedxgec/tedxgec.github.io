006639d958d9add53e0948d3ffe4d7ab67f24089
Saheel <saheelwagh@Gmail.com>
2017-07-18 12:53:50 +0530

    added tedx descr to homepage

--- scripts/init.js
+++ scripts/init.js
@@ -2,4 +2,5 @@ $( document).ready(function(){
     $(".dropdown-button").dropdown();
     $('.carousel').carousel();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.slider').slider();
 });
